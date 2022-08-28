606f459a30f4be2916bb09d8971727176ac8f8f8
Dario Cangialosi Arkenidar (DCA) <dario.cangialosi@gmail.com>
2022-08-28 16:50:12 +0200

    adapted from pgsql to mysql

---
+++ mysql-db-init.sql
@@ -0,0 +1,7 @@
+
+-- table
+CREATE TABLE users_login (user_name text);
+
+-- records
+INSERT INTO users_login (user_name) VALUES ("username1");
+INSERT INTO users_login (user_name) VALUES ("username2");
